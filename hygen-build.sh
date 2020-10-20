506838d43d4e39710debcff04ca6cada09ce4542
Paul Wright <pwright@redhat.com>
2020-10-20 17:28:45 +0100

    update markdown templates

--- hygen-build.sh
+++ hygen-build.sh
@@ -1,9 +1,9 @@
 cat hygen.md > hygen.didact.md
 
-hygen didact markdown --termname hygen --file hygen --location 'Globally with npm' --name Install --cmd npm%20i%20-g%20hygen
+hygen markdown command --termname hygen --file hygen --location 'Globally with npm' --name Install --cmd npm%20i%20-g%20hygen
 
-hygen didact markdown --termname hygen --file hygen --location 'quick run-down to get to your own generator' --name Create-new-generator --cmd mkdir%20hygen-test%3B%20cd%20hygen-test%3B%20hygen%20init%20self%3B%20hygen%20generator%20new%20awesome-generator%3B%20hygen%20awesome-generator%20new%20hello
+hygen markdown command --termname hygen --file hygen --location 'quick run-down to get to your own generator' --name Create-new-generator --cmd mkdir%20hygen-test%3B%20cd%20hygen-test%3B%20hygen%20init%20self%3B%20hygen%20generator%20new%20awesome-generator%3B%20hygen%20awesome-generator%20new%20hello
 
-hygen didact markdown --termname hygen --file hygen --location 'builds a new generator for you' --name New-generator --cmd hygen%20generator%20new%20generatorName
+hygen markdown command --termname hygen --file hygen --location 'builds a new generator for you' --name New-generator --cmd hygen%20generator%20new%20generatorName
 
  
